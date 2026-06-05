7642ca4a0700ea92617d38bb4bebbc91c2a53b68
AlyssaDambeck <al667195@ucf.edu>
2026-06-05 07:32:31 -0400

    added add and delete contact
    added js script for it

--- js/contacts.js
+++ js/contacts.js
@@ -21,6 +21,8 @@ window.onload = function()
         firstName +
         " " +
         lastName;
+
+    searchContact();
 };
 
 function doLogout()
@@ -29,6 +31,56 @@ function doLogout()
 
     window.location.href =
         "index.html";
+}
+
+function displayContacts(results)
+{
+    let grid =
+        document.getElementById(
+            "contactsGrid"
+        );
+
+    grid.innerHTML = "";
+
+    for(let i = 0; i < results.length; i++)
+    {
+        let contact =
+            results[i];
+
+        grid.innerHTML +=
+        `
+        <div class="contact-card">
+
+            <h3>
+                ${contact.FirstName}
+                ${contact.LastName}
+            </h3>
+
+            <p>
+                ${contact.PhoneNumber}
+            </p>
+
+            <p>
+                ${contact.EmailAddress}
+            </p>
+
+            <div class="actions">
+
+                <button
+                    onclick="editContact(${contact.ID})">
+                    Edit
+                </button>
+
+                <button
+                    onclick="deleteContact(${contact.ID})">
+                    Delete
+                </button>
+
+            </div>
+
+        </div>
+        `;
+    }
 }
 
 function searchContact()
@@ -69,9 +121,22 @@ function searchContact()
         )
         {
             let jsonObject =
-                JSON.parse(xhr.responseText);
+                JSON.parse(
+                    xhr.responseText
+                );
 
-            console.log(jsonObject.results);
+            if(jsonObject.error != "")
+            {
+                alert(
+                    jsonObject.error
+                );
+
+                return;
+            }
+
+            displayContacts(
+                jsonObject.results
+            );
         }
     };
 
@@ -80,9 +145,125 @@ function searchContact()
 
 function addContact()
 {
-    alert("Add Contact clicked.");
+    let firstName =
+        prompt(
+            "First Name"
+        );
 
-    /*
-        MODAL OR API CODE HERE
-    */
+    let lastName =
+        prompt(
+            "Last Name"
+        );
+
+    let phoneNumber =
+        prompt(
+            "Phone Number"
+        );
+
+    let emailAddress =
+        prompt(
+            "Email Address"
+        );
+
+    let tmp =
+    {
+        firstName:
+            firstName,
+
+        lastName:
+            lastName,
+
+        phoneNumber:
+            phoneNumber,
+
+        emailAddress:
+            emailAddress,
+
+        userId:
+            parseInt(userId)
+    };
+
+    let jsonPayload =
+        JSON.stringify(tmp);
+
+    let url =
+        urlBase +
+        "/AddContact.php";
+
+    let xhr =
+        new XMLHttpRequest();
+
+    xhr.open(
+        "POST",
+        url,
+        true
+    );
+
+    xhr.setRequestHeader(
+        "Content-type",
+        "application/json; charset=UTF-8"
+    );
+
+    xhr.onreadystatechange =
+    function()
+    {
+        if(
+            this.readyState == 4 &&
+            this.status == 200
+        )
+        {
+            searchContact();
+        }
+    };
+
+    xhr.send(
+        jsonPayload
+    );
+}
+
+function deleteContact(id)
+{
+    let tmp =
+    {
+        id: id,
+        userId:
+            parseInt(userId)
+    };
+
+    let jsonPayload =
+        JSON.stringify(tmp);
+
+    let url =
+        urlBase +
+        "/DeleteContact.php";
+
+    let xhr =
+        new XMLHttpRequest();
+
+    xhr.open(
+        "POST",
+        url,
+        true
+    );
+
+    xhr.setRequestHeader(
+        "Content-type",
+        "application/json; charset=UTF-8"
+    );
+
+    xhr.onreadystatechange =
+    function()
+    {
+        if(
+            this.readyState == 4 &&
+            this.status == 200
+        )
+        {
+            searchContact();
+        }
+    };
+
+    xhr.send(
+        jsonPayload
+    );
 }

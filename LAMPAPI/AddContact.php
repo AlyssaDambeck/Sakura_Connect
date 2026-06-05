76b2071767f834338a2616cf4b8b618b20838fea
Sean Angelil <Seanangelil0413@gmail.com>
2026-06-05 19:27:44 -0400

    Refactor AddContact.php for error handling and variable names

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,10 +1,13 @@
 <?php
 
-
-header('Access-Control-Allow-Origin: https://sakuraconnect.online');
+/*ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+*/
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Credentials: false');
 
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS')
@@ -16,11 +19,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS')
 
 $inData = getRequestInfo();
 
-$firstName = $inData["firstName"];
-$lastName = $inData["lastName"];
-$phoneNumber = $inData["phoneNumber"];
-$emailAddress = $inData["emailAddress"];
-$userId = $inData["userId"];
+//echo json_encode($inData);
+//exit();
+
+$firstName = isset($inData["FirstName"]) ? $inData["FirstName"] : "";
+$lastName = isset($inData["LastName"]) ? $inData["LastName"] : "" ;
+$phoneNumber = isset($inData["PhoneNumber"]) ? $inData["PhoneNumber"] : "" ;
+$emailAddress = isset($inData["EmailAddress"]) ? $inData["EmailAddress"] : "" ;
+$userID = isset($inData["UserID"]) ? $inData["UserID"] : "" ;
 
 
 $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "SakuraConnect");
@@ -45,17 +51,18 @@ $stmt->bind_param(
 	$lastName,
 	$phoneNumber,
 	$emailAddress,
-	$userId
+	$userID
 );
 
-if ($stmt->execute())
-{
-	returnWithError("");
-}
-else
-{
-	returnWithError($stmt->error);
-}
+try {
+    if ($stmt->execute()) {
+        returnWithError("");
+    } else {
+        returnWithError($stmt->error);
+    }
+} catch (mysqli_sql_exception $e) {
+	returnWithError($e->getMessage());
+	}
 
 
 $stmt->close();
